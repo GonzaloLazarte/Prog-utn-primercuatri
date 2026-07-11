6d261e4078e90d4f99dbe1f4225d0842f59819c0
Gonzalo_Lazarte <lazarte.gonzalo7000@gmail.com>
2026-07-10 21:02:51 -0300

    se_añade_EJ12

---
+++ C#_funciones/EJ12/EJ12/Calcular.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ12 {
+    internal class Calcular {
+        private int suma, mensual,aux;
+        
+        public Calcular() {
+            mensual = 10;
+            aux = 0;
+            suma = 0;
+        }
+        public void Ejecutar() {
+            for (int i = 1; i <= 12; i++) {
+                aux = mensual;
+                Console.WriteLine($"total a pagar en el mes {i}: ${mensual}");
+                suma += mensual;
+                mensual = aux * 2;
+            }
+            Console.WriteLine($"SUMA TOTAL DE LOS MESES PAGADOS: $ {suma}");
+        }
+
+
+    }
+}
---
+++ C#_funciones/EJ12/EJ12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Una persona adquirió un producto para pagar en 20 meses. El primer mes
+//pagó $10, el segundo $20, el tercero $40 y así sucesivamente. Realice un
+//algoritmo para determinar cuánto debe pagar mensualmente y el total de
+//lo que pagó después de los 10 meses. Resolver el ejercicio con una clase
+//y los métodos que sean necesarios.
+
+namespace EJ12 {
+    internal class Program {
+        static void Main(string[] args) {
+            Calcular calculo = new Calcular();
+            calculo.Ejecutar();
+
+        }
+    }
+}
