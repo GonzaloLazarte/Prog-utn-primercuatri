679c1e77299495abe7d42421f952fc4ab05b2dd8
Gonzalo_Lazarte <lazarte.gonzalo7000@gmail.com>
2026-07-26 22:30:31 -0300

    se_añade_EJ13

---
+++ C#_funciones/EJ13/EJ13/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+//Una empresa les paga a sus empleados con base en las horas trabajadas en
+//la semana. Realice un algoritmo para determinar el sueldo semanal de N
+//trabajadores y, además, calcule cuánto pagó la empresa por los N empleados. Usando una clase
+// con los métodos que crea conveniente.
+
+namespace EJ13 {
+    internal class Program {
+        static int numEmpleados = 0;
+        static void Main(string[] args) {
+            Console.WriteLine("============= CALCULADORA DE SUELDOS PARA EMPLEADOS =============");
+            Console.WriteLine("");
+            validar();
+            Salarios salarioEmpleados = new Salarios(numEmpleados);
+        }
+        static void validar() {
+            int num = 0;
+            while (true) {
+                Console.Write("INGRESE EL NUMERO DE EMPLEADOS: ");
+                if (int.TryParse(Console.ReadLine(), out num) && num > 0) {
+                    numEmpleados = num;
+                    break;
+                }
+                else {
+                    Console.WriteLine("error tipo de dato incorrecto");
+                }
+            }
+        }
+    }
+}
---
+++ C#_funciones/EJ13/EJ13/Salarios.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EJ13 {
+    internal class Salarios {
+        private int horasTrabajadas = 0,horasTrabajador = 0;
+        private double sueldoPorHora = 0;
+        public Salarios(int num) {
+            validarPagoHora();
+            cargarHoras(num);
+            mostrarTotal();
+        }
+        private void cargarHoras(int _num) {
+            int horas = 0;
+            for (int i = 0; i < _num; i++) {
+                while (true) {
+                Console.WriteLine("");
+                Console.Write($"INGRESE HORAS SEMANALES DEL {i+1}° EMPLEADO: ");
+                    if (int.TryParse(Console.ReadLine(), out horas) && horas >= 1) {
+                        horasTrabajadas += horas;
+                        horasTrabajador = horas;
+                        mostrarSalarioEmpleado(i);
+                        break;
+                    }
+                    else {
+                        Console.WriteLine("error tipo de dato incorrecto");
+                    }
+                }
+            }
+        }
+        private void validarPagoHora() {
+            double pago = 0;
+            while (true) {
+                Console.Write("INGRESE EL PAGO POR HORA: ");
+                if (double.TryParse(Console.ReadLine(), out pago) && pago >= 1) {
+                    sueldoPorHora += pago;
+                    break;
+                }
+                else {
+                    Console.WriteLine("error tipo de dato incorrecto");
+                }
+            }
+        }
+        private void mostrarSalarioEmpleado(int _i) {
+            Console.WriteLine($"El empleado n° {_i + 1} tiene  un sueldo de: ${horasTrabajador * sueldoPorHora} por trabajar {horasTrabajador}");
+        }
+        private void mostrarTotal() {
+            Console.WriteLine($"la empresa pago un total de: ${horasTrabajadas * sueldoPorHora} por la cantidad de {horasTrabajadas} hs. trabajadas por todos sus empleados");
+        }
+    }
+}
